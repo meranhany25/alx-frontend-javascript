c4d9b166860a7a78e30e8f3e1b7d3bb99ad55bdf
meranhan25 <meranhany25@gmail.com>
2024-05-29 23:34:08 +0300

    m

--- 0x01-ES6_promise/.eslintrc.js
+++ 0x01-ES6_promise/.eslintrc.js
@@ -1,35 +1,34 @@
-module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
-    },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: ['jest'],
-    rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
+export const env = {
+    browser: false,
+    es6: true,
+    jest: true,
+};
+export const extends = [
+    'airbnb-base',
+    'plugin:jest/all',
+];
+export const globals = {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+};
+export const parserOptions = {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+};
+export const plugins = ['jest'];
+export const rules = {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
         'error',
         'LabeledStatement',
         'WithStatement',
-      ],
-    },
-    overrides: [
-      {
+    ],
+};
+export const overrides = [
+    {
         files: ['*.js'],
         excludedFiles: 'babel.config.js',
-      },
-    ],
-  };
+    },
+];
+
