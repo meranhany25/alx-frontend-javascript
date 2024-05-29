cd2f8995367475dffd249956a17526f1d7986294
meranhan25 <meranhany25@gmail.com>
2024-05-29 23:35:31 +0300

    m

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,5 @@
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
       if (success === true) {
         resolve({
           status: 200,
@@ -9,4 +9,4 @@ export default function getFullResponseFromAPI(success) {
         reject(new Error('The fake API is not working currently'));
       }
     });
-  }
+}
