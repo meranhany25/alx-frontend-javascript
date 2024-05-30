e416eb82e425587595074b2b753a488901a77f4c
meranhan25 <meranhany25@gmail.com>
2024-05-30 21:10:23 +0300

    m

---
+++ 0x02-ES6_classes/2-main.js
@@ -0,0 +1,16 @@
+import HolbertonCourse from './2-hbtn_course.js';
+
+const c1 = new HolbertonCourse('ES6', 1, ['Bob', 'Jane']);
+console.log(c1.name);
+c1.name = 'Python 101';
+console.log(c1);
+
+try {
+  c1.name = 12;
+} catch (err) {
+  console.log(err);
+}
+
+try { /* empty */ } catch (err) {
+  console.log(err);
+}
